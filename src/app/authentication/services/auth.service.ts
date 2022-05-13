80b2eb914aa04a899be4829322547010eccb4ed4
Mario Neubacher <neubacher.mario00@gmail.com>
2022-05-13 14:35:43 +0200

    fixed router url bug

--- src/app/authentication/services/auth.service.ts
+++ src/app/authentication/services/auth.service.ts
@@ -128,7 +128,7 @@ export class AuthService {
   SignOut() {
     return this.afAuth.signOut().then(() => {
       localStorage.removeItem('user');
-      this.router.navigate(['sign-in']);
+      this.router.navigate(['']);
     });
   }
 }
